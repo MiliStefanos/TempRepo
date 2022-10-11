13424dbf6f5dcef77f2b48d5a1c77eba1cd5cc2f
milistefanos@hotmail.com <milistefanos@hotmail.com>
2022-10-11 10:55:54 +0300

    Ask for Review before push

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ using namespace std;
 int main()
 {
     cout << "Let's see the world through our hands" << endl;
+    cout << "Try to ask for Kostas acceptance in order to push" << endl;
     return 0;
 }
